6a4877e96ba1ba28a6091509499c46dcab551fb4
DESKTOP-CASA\Pesonal <juankortiz@proton.me>
2024-09-01 20:28:12 -0300

    Crear vista de usuarios

---
+++ resources/views/usuarios.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Listado de usuarios</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <div class="contenedor">
+        <h1>Listado de usuarios</h1>
+
+        <ul class="lista-elementos">
+            <li>Bill Gates</li>
+            <li>Paul McCartney</li>
+            <li>Paula Redondo</li>
+            <li>Diego Maradona</li>
+        </ul>
+    </div>
+</body>
+</html>
