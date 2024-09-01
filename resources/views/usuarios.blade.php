a9ef76dc96353305c20e954f95786f14236d4a90
DESKTOP-CASA\Pesonal <juankortiz@proton.me>
2024-09-01 20:29:34 -0300

    Volver a utilizar estilos de TailwindCSS

--- resources/views/usuarios.blade.php
+++ resources/views/usuarios.blade.php
@@ -8,14 +8,14 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <div class="contenedor">
-        <h1>Listado de usuarios</h1>
+    <div class="m-5">
+        <h1 class="text-3xl font-bold text-blue-900">Listado de usuarios</h1>
 
-        <ul class="lista-elementos">
-            <li>Bill Gates</li>
-            <li>Paul McCartney</li>
-            <li>Paula Redondo</li>
-            <li>Diego Maradona</li>
+        <ul class="mt-4">
+            <li class="text-blue-800 text-lg">Bill Gates</li>
+            <li class="text-blue-800 text-lg">Paul McCartney</li>
+            <li class="text-blue-800 text-lg">Paula Redondo</li>
+            <li class="text-blue-800 text-lg">Diego Maradona</li>
         </ul>
     </div>
 </body>
