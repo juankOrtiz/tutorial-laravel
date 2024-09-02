46044820d4a2687311d9088705b94b19f19d4835
DESKTOP-CASA\Pesonal <juankortiz@proton.me>
2024-09-01 23:07:42 -0300

    Limpiar layout

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::resource('usuarios', UsuariosController::class);
 
